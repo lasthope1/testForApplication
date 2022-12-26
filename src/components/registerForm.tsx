45625c40ff0c965f63d33d23b0090a0f80b5048c
lasthope1 <divinity3191@gmail.com>
2022-12-26 17:06:41 +0700

    second comment

--- src/components/registerForm.tsx
+++ src/components/registerForm.tsx
@@ -1,17 +1,19 @@
 import {useRef, useState, useEffect} from 'react'
-import {useTelephone, countries} from 'use-telephone'
 
 function RegisterForm() {
-    const telephone = useTelephone();
 
     const [telNum, setTelNum] = useState<string>('');
     const [postCode, setPostCode] = useState<string>('');
+    const [gender, setGender] = useState<string>('');
+    const [accept, setAccept] = useState<boolean>(false);
 
   return (
-        <div className="mt-8 bg-white px-10 py-10 rounded-3xl border-2 border-gray-100 shadow-lg">
-            <div className="w-full ">
-                <form>
-                    <div className='mt-3'>
+        <div className="w-full mt-8 bg-white px-5 py-10 rounded-3xl border-2 border-gray-100 shadow-lg">
+            <form>
+                <div className='w-full flex'>
+                <div className='lg:w-1/2 px-5'>
+                    <h3 className='text-base font-semibold text-sky-500'>General Information</h3>
+                    <div className='mt-5'>
                         <label className="text-sm font-medium text-gray-700 undefined">
                             First Name
                         </label>
@@ -25,7 +27,7 @@ function RegisterForm() {
                             />
                         </div>
                     </div>
-                    <div className='mt-3'>
+                    <div className='mt-4'>
                         <label className="text-sm font-medium text-gray-700 undefined">
                             Last Name
                         </label>
@@ -39,6 +41,25 @@ function RegisterForm() {
                             />
                         </div>
                     </div>
+                    <div className="mt-4">
+                        <label className="block text-sm font-medium text-gray-700 undefined">
+                            Gender
+                        </label>
+                        <div className='flex flex-col items-start'>
+                            <select className='w-full mt-2 p-2 border-2 border-gray-300 rounded-md shadow-sm bg-transparent
+                                focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+                                onChange={e => {setGender(e.target.value)}}
+                                required
+                            >
+                                <option value='mail'>Mail</option>
+                                <option value='femail'>Femail</option>
+                                <option value='other'>Other</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className='lg:w-1/2 px-5'>
+                    <h3 className='text-base font-semibold text-green-500'>Contact Details</h3>
                     <div className="mt-4">
                         <label className="block text-sm font-medium text-gray-700 undefined">
                             Email
@@ -99,18 +120,33 @@ function RegisterForm() {
                             />
                         </div>
                     </div>
-                    <div className="flex flex-col mt-7">
+                </div>
+                </div>
+                <div className="mt-7 flex justify-center gap-2">
+                    <div>
+                        <input name="accept_cond" 
+                            className='lg:w-5 lg:h-5 hover:cursor-pointer'
+                            type="checkbox"
+                            defaultChecked={accept}
+                            onChange={() => setAccept(!accept)}
+                            required
+                        />
+                    </div>
+                    <label className="flex text-sm font-medium text-gray-700 undefined">
+                        Accept terms and conditions
+                    </label>
+                </div>
+                <div className="flex flex-col mt-4">
                         <button 
                             className="active:scale-[.98] active:duration hover:scale-[1.01] ease-in-out translate-all py-3 rounded-xl 
                             bg-gradient-to-tr from-sky-600 to-green-400 text-white text-lg font-bold shadow-lg shadow-blue-500/50">
                             SIGN UP
                         </button>
                     </div>
-                </form>
-                <div className="mt-4 flex justify-center items-center">
-                    <p className="font-medium text-base text-gray-500">Already have an account ?</p>
-                    <a className="text-sky-600 text-base font-medium ml-2 hover:underline" href="/">Sign in</a>
-                </div>
+            </form>
+            <div className="mt-4 flex justify-center items-center">
+                <p className="font-medium text-base text-gray-500">Already have an account ?</p>
+                <a className="text-sky-600 text-base font-medium ml-2 hover:underline" href="/">Sign in</a>
             </div>
         </div>
   )
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -4,7 +4,7 @@ import RegisterForm from '../components/registerForm'
 function RegisterPage() {
   return (
     <div className='flex w-full h-screen items-center justify-center'>
-        <div className='lg:w-1/3'>
+        <div className='lg:w-1/2'>
             <h3 className='flex justify-center mt-3 text-4xl font-semibold'>Let's being our family !</h3>
             <RegisterForm />
         </div>
